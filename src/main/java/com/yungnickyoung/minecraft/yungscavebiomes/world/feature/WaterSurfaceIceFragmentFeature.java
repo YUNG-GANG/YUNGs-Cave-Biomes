06b56d3590ec9e9c1c430621a3f940ae3c0996e9
KdotJPG <KdotJPG@users.noreply.github.com>
2022-03-29 17:41:58 -0400

    Reduce GC in WaterSurfaceIceFragmentFeature using ThreadLocal.
    Also slightly changes how the noise seed is extracted from the world seed.

--- src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/world/feature/WaterSurfaceIceFragmentFeature.java
+++ src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/world/feature/WaterSurfaceIceFragmentFeature.java
@@ -11,6 +11,7 @@ import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 
 public class WaterSurfaceIceFragmentFeature extends Feature<NoneFeatureConfiguration> {
@@ -18,8 +19,11 @@ public class WaterSurfaceIceFragmentFeature extends Feature<NoneFeatureConfigura
         super(codec);
     }
 
+    // Flood-fill configuration.
     private static final int MAX_FLOOD_RADIUS = 8;
     private static final int MAX_FLOOD_DIAMETER = MAX_FLOOD_RADIUS * 2 + 1;
+
+    // Ice fragment cellular noise parameters.
     private static final float ICE_FRAGMENT_SEPARATION = 0.15f;
     private static final float ICE_FRAGMENT_NOISE_FREQUENCY = 1.0f / 12.0f;
 
@@ -31,66 +35,91 @@ public class WaterSurfaceIceFragmentFeature extends Feature<NoneFeatureConfigura
         WorldGenLevel level = context.level();
         BlockPos origin = context.origin();
 
-        // Set up for flood-fill over the ice surface shard noise
-        Queue<BlockPos> floodQueue = new ArrayDeque<>(MAX_FLOOD_DIAMETER * MAX_FLOOD_DIAMETER);
-        boolean[] visited = new boolean[MAX_FLOOD_DIAMETER * MAX_FLOOD_DIAMETER];
-
-        // Start flood-fill the feature origin
-        floodQueue.add(origin);
-
-        // TODO this can be moved to a (thread-safe) static initialization
-        FastNoise iceFragmentNoise = new FastNoise((int)(level.getSeed() >> 32));
-        iceFragmentNoise.SetCellularDistanceFunction(FastNoise.CellularDistanceFunction.Euclidean);
-        iceFragmentNoise.SetCellularReturnType(FastNoise.CellularReturnType.Distance2Sub);
-        iceFragmentNoise.SetFrequency(ICE_FRAGMENT_NOISE_FREQUENCY);
-
-        // Perform the flood-fill
-        while (!floodQueue.isEmpty()) {
-            BlockPos currentPos = floodQueue.poll();
-            BlockState currentState = level.getBlockState(currentPos);
-
-            // Nothing to do here if it's not water or ice.
-            if (!currentState.is(Blocks.WATER) && !currentState.is(Blocks.ICE)) continue;
-
-            // TODO do we need to check if it's not a water-logged block?
-
-            // 3D noise so two pools offset only vertically don't show identical patterns.
-            float iceShardNoiseValue = iceFragmentNoise.GetCellular(currentPos.getX(), currentPos.getY(), currentPos.getZ());
-
-            // If we're in the boundary between two ice shards, nothing to do here.
-            if (iceShardNoiseValue <= (ICE_FRAGMENT_SEPARATION - 1)) continue;
-
-            // We know this one is ice now.
-            level.setBlock(currentPos, Blocks.ICE.defaultBlockState(), 2);
-
-            // Now consider propagating into the four neighbors.
-            considerNeighbor(floodQueue, visited, origin, currentPos.east());
-            considerNeighbor(floodQueue, visited, origin, currentPos.west());
-            considerNeighbor(floodQueue, visited, origin, currentPos.south());
-            considerNeighbor(floodQueue, visited, origin, currentPos.north());
-        }
+        // Call the thread-local allocation to perform the flood-fill.
+        iceFragmentFillerThreadLocal.get().execute(origin, level);
 
         return false;
     }
 
-    private void considerNeighbor(Queue<BlockPos> floodQueue, boolean[] visited, BlockPos origin, BlockPos neighborPos) {
+    private static final ThreadLocal<IceFragmentFiller> iceFragmentFillerThreadLocal = new ThreadLocal<>() {
+        @Override
+        protected IceFragmentFiller initialValue() {
+            return new IceFragmentFiller();
+        }
+    };
 
-        // Delta from feature placement center.
-        int dx = neighborPos.getX() - origin.getX();
-        int dz = neighborPos.getZ() - origin.getZ();
+    private static class IceFragmentFiller {
+        private final Queue<BlockPos> floodQueue;
+        private final boolean[] visited;
+        private final FastNoise iceFragmentNoise;
 
-        // Initial out-of-range check.
-        if (dx < -MAX_FLOOD_RADIUS || dx > MAX_FLOOD_RADIUS || dz < -MAX_FLOOD_RADIUS || dz > MAX_FLOOD_RADIUS) return;
+        public IceFragmentFiller() {
+            this.floodQueue = new ArrayDeque<>(MAX_FLOOD_DIAMETER * MAX_FLOOD_DIAMETER);
+            this.visited = new boolean[MAX_FLOOD_DIAMETER * MAX_FLOOD_DIAMETER];
+            this.iceFragmentNoise = new FastNoise();
+            this.iceFragmentNoise.SetCellularDistanceFunction(FastNoise.CellularDistanceFunction.Euclidean);
+            this.iceFragmentNoise.SetCellularReturnType(FastNoise.CellularReturnType.Distance2Sub);
+            this.iceFragmentNoise.SetFrequency(ICE_FRAGMENT_NOISE_FREQUENCY);
+        }
 
-        // Don't add to the queue if it's already been visited. Otherwise mark it as such.
-        int visitedIndex = (dz + MAX_FLOOD_RADIUS) * MAX_FLOOD_DIAMETER + (dx + MAX_FLOOD_RADIUS);
-        if (visited[visitedIndex]) return;
-        visited[visitedIndex] = true;
+        public void execute(BlockPos origin, WorldGenLevel level) {
 
-        // Keep out-of-range borders looking nice in all directions by bounding the search to a circle.
-        if (dx * dx + dz * dz > EFFECTIVE_MAX_RANGE_RADIUS_SQ) return;
+            // Initialize for a new flood-fill over the ice surface shard noise
+            this.floodQueue.clear();
+            Arrays.fill(visited, false);
 
-        // Now it's ready to be processed at some point.
-        floodQueue.add(neighborPos);
+            // Set/update the noise seed. Typically this will only happen once,
+            // unless the feature is being used simultaneously by multiple generators.
+            int noiseSeed = (int)level.getSeed();
+            if (this.iceFragmentNoise.GetSeed() != noiseSeed) this.iceFragmentNoise.SetSeed(noiseSeed);
+
+            // Start flood-fill at the feature origin
+            this.floodQueue.add(origin);
+
+            // Perform the flood-fill
+            while (!this.floodQueue.isEmpty()) {
+                BlockPos currentPos = this.floodQueue.poll();
+                BlockState currentState = level.getBlockState(currentPos);
+
+                // Nothing to do here if it's not water or ice.
+                if (!currentState.is(Blocks.WATER) && !currentState.is(Blocks.ICE)) continue;
+
+                // 3D noise so two pools offset only vertically don't show identical patterns.
+                float iceShardNoiseValue = this.iceFragmentNoise.GetCellular(currentPos.getX(), currentPos.getY(), currentPos.getZ());
+
+                // If we're in the boundary between two ice shards, nothing to do here.
+                if (iceShardNoiseValue <= (ICE_FRAGMENT_SEPARATION - 1)) continue;
+
+                // We know this one is ice now.
+                level.setBlock(currentPos, Blocks.ICE.defaultBlockState(), 2);
+
+                // Now consider propagating into the four neighbors.
+                considerNeighbor(origin, currentPos.east());
+                considerNeighbor(origin, currentPos.west());
+                considerNeighbor(origin, currentPos.south());
+                considerNeighbor(origin, currentPos.north());
+            }
+        }
+
+        private void considerNeighbor(BlockPos origin, BlockPos neighborPos) {
+
+            // Delta from feature placement center.
+            int dx = neighborPos.getX() - origin.getX();
+            int dz = neighborPos.getZ() - origin.getZ();
+
+            // Initial out-of-range check.
+            if (dx < -MAX_FLOOD_RADIUS || dx > MAX_FLOOD_RADIUS || dz < -MAX_FLOOD_RADIUS || dz > MAX_FLOOD_RADIUS) return;
+
+            // Don't add to the queue if it's already been visited. Otherwise mark it as such.
+            int visitedIndex = (dz + MAX_FLOOD_RADIUS) * MAX_FLOOD_DIAMETER + (dx + MAX_FLOOD_RADIUS);
+            if (this.visited[visitedIndex]) return;
+            this.visited[visitedIndex] = true;
+
+            // Keep out-of-range borders looking nice in all directions by bounding the search to a circle.
+            if (dx * dx + dz * dz > EFFECTIVE_MAX_RANGE_RADIUS_SQ) return;
+
+            // We've figured out we do want to process this position in the flood-fill.
+            this.floodQueue.add(neighborPos);
+        }
     }
 }
