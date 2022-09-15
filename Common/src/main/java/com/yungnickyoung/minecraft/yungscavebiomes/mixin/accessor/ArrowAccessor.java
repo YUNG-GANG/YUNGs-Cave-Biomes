9ec3757ac6434647b5c19df492746bcd1d63a425
Nick Young <yungnickyoung@gmail.com>
2022-09-14 21:19:56 -0400

    Couple extra things I forgot to include in previous commit

--- Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/mixin/accessor/ArrowAccessor.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/mixin/accessor/ArrowAccessor.java
@@ -1,18 +1,12 @@
 package com.yungnickyoung.minecraft.yungscavebiomes.mixin.accessor;
 
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.projectile.Arrow;
 import net.minecraft.world.item.alchemy.Potion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-import java.util.Set;
-
 @Mixin(Arrow.class)
 public interface ArrowAccessor {
-    @Accessor
-    Set<MobEffectInstance> getEffects();
-
     @Accessor
     Potion getPotion();
 }
