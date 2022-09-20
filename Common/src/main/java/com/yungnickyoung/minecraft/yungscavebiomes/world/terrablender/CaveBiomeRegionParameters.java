c14917cc2d8406db6b7a61a8ee172b05a502d8c9
Nick Young <yungnickyoung@gmail.com>
2022-09-20 15:13:14 -0400

    Move biome climate parameters into single source of truth.

---
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/world/terrablender/CaveBiomeRegionParameters.java
@@ -0,0 +1,55 @@
+package com.yungnickyoung.minecraft.yungscavebiomes.world.terrablender;
+
+import net.minecraft.world.level.biome.Climate;
+
+/**
+ * Parameters used for creating TerraBlender region.
+ * The actual creation of TerraBlender region is handled in loader-dependent modules.
+ */
+public class CaveBiomeRegionParameters {
+    // Duplicate of OverworldBoimeBuilder fields
+    private static final Climate.Parameter[] temperatures = new Climate.Parameter[]{
+            Climate.Parameter.span(-1.0F, -0.45F),
+            Climate.Parameter.span(-0.45F, -0.15F),
+            Climate.Parameter.span(-0.15F, 0.2F),
+            Climate.Parameter.span(0.2F, 0.55F),
+            Climate.Parameter.span(0.55F, 1.0F)
+    };
+    private static final Climate.Parameter fullRange = Climate.Parameter.span(-1.0F, 1.0F);
+    private static final Climate.Parameter frozenRange = temperatures[0];
+    private static final Climate.Parameter unfrozenRange = Climate.Parameter.span(temperatures[1], temperatures[4]);
+
+    // Dripstone modifications
+    public static final Climate.ParameterPoint DRIPSTONE_OLD = Climate.parameters(fullRange, fullRange, Climate.Parameter.span(0.8F, 1.0F), fullRange, Climate.Parameter.span(0.2F, 0.9F), fullRange, 0.0F);
+    public static final Climate.ParameterPoint DRIPSTONE_NEW = Climate.parameters(unfrozenRange, fullRange, Climate.Parameter.span(0.8F, 1.0F), fullRange, Climate.Parameter.span(0.2F, 0.9F), fullRange, 0.0F);
+
+    // Cave biomes
+    public static final Climate.ParameterPoint FROSTED_CAVES = Climate.parameters(
+            frozenRange, // temperature
+            fullRange, // humidity
+//                    Climate.Parameter.span(0.6F, 1.0F), // continentalness
+            Climate.Parameter.span(0.1F, 1.0F), // continentalness
+            fullRange, // erosion
+            Climate.Parameter.span(0.2F, 0.9F), // depth
+            fullRange, // weirdness
+            0.0F // offset
+    );
+    public static final Climate.ParameterPoint MARBLE_CAVES = Climate.parameters(
+            frozenRange, // temperature
+            fullRange, // humidity
+            Climate.Parameter.span(-1.05F, -0.19F), // continentalness
+            fullRange, // erosion
+            Climate.Parameter.span(0.2F, 0.9F), // depth
+            fullRange, // weirdness
+            0.0F // offset
+    );
+    public static final Climate.ParameterPoint ANCIENT_CAVES = Climate.parameters(
+            temperatures[4], // temperature
+            fullRange, // humidity
+            Climate.Parameter.span(0.2F, 0.7F), // continentalness
+            Climate.Parameter.span(0.2F, 1.0F), // erosion
+            Climate.Parameter.span(0.2F, 0.9F), // depth
+            fullRange, // weirdness
+            0.0F // offset
+    );
+}
--- Fabric/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/world/CaveBiomeRegion.java
+++ Fabric/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/world/CaveBiomeRegion.java
@@ -3,6 +3,7 @@ package com.yungnickyoung.minecraft.yungscavebiomes.world;
 import com.mojang.datafixers.util.Pair;
 import com.yungnickyoung.minecraft.yungscavebiomes.YungsCaveBiomesCommon;
 import com.yungnickyoung.minecraft.yungscavebiomes.module.BiomeModule;
+import com.yungnickyoung.minecraft.yungscavebiomes.world.terrablender.CaveBiomeRegionParameters;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
@@ -14,18 +15,6 @@ import terrablender.api.RegionType;
 import java.util.function.Consumer;
 
 public class CaveBiomeRegion extends Region {
-    // Duplicate of OverworldBoimeBuilder params
-    private final Climate.Parameter[] temperatures = new Climate.Parameter[]{
-            Climate.Parameter.span(-1.0F, -0.45F),
-            Climate.Parameter.span(-0.45F, -0.15F),
-            Climate.Parameter.span(-0.15F, 0.2F),
-            Climate.Parameter.span(0.2F, 0.55F),
-            Climate.Parameter.span(0.55F, 1.0F)
-    };
-    private final Climate.Parameter FULL_RANGE = Climate.Parameter.span(-1.0F, 1.0F);
-    private final Climate.Parameter FROZEN_RANGE = this.temperatures[0];
-    private final Climate.Parameter UNFROZEN_RANGE = Climate.Parameter.span(this.temperatures[1], this.temperatures[4]);
-
     public CaveBiomeRegion(ResourceLocation name, RegionType type, int weight) {
         super(name, type, weight);
     }
@@ -36,28 +25,17 @@ public class CaveBiomeRegion extends Region {
         addModifiedVanillaOverworldBiomes(mapper, b -> {
             // Modify dripstone caves to only spawn in the unfrozen temperature range.
             // The frozen range will be occupied by frosted caves
-            b.replaceParameter(
-                    Climate.parameters(this.FULL_RANGE, this.FULL_RANGE, Climate.Parameter.span(0.8F, 1.0F), this.FULL_RANGE, Climate.Parameter.span(0.2F, 0.9F), this.FULL_RANGE, 0.0F),
-                    Climate.parameters(this.UNFROZEN_RANGE, this.FULL_RANGE, Climate.Parameter.span(0.8F, 1.0F), this.FULL_RANGE, Climate.Parameter.span(0.2F, 0.9F), this.FULL_RANGE, 0.0F));
+            b.replaceParameter(CaveBiomeRegionParameters.DRIPSTONE_OLD, CaveBiomeRegionParameters.DRIPSTONE_NEW);
         });
 
         if (YungsCaveBiomesCommon.FROSTED_CAVES_ENABLED) {
-            this.addBiome(mapper,
-                    this.FROZEN_RANGE, // temperature
-                    this.FULL_RANGE, // humidity
-//                    Climate.Parameter.span(0.6F, 1.0F), // continentalness
-                    Climate.Parameter.span(0.1F, 1.0F), // continentalness
-                    this.FULL_RANGE, // erosion
-                    this.FULL_RANGE, // weirdness
-                    Climate.Parameter.span(0.2F, 0.9F), // depth
-                    0.0F, // offset
-                    BiomeModule.FROSTED_CAVES.getResourceKey());
+            this.addBiome(mapper, CaveBiomeRegionParameters.FROSTED_CAVES, BiomeModule.FROSTED_CAVES.getResourceKey());
         }
         if (YungsCaveBiomesCommon.MARBLE_CAVES_ENABLED) {
-            this.addBiome(mapper, this.FROZEN_RANGE, this.FULL_RANGE, Climate.Parameter.span(-1.05F, -0.19F), this.FULL_RANGE, this.FULL_RANGE, Climate.Parameter.span(0.2F, 0.9F), 0.0F, BiomeModule.MARBLE_CAVES.getResourceKey());
+            this.addBiome(mapper, CaveBiomeRegionParameters.MARBLE_CAVES, BiomeModule.MARBLE_CAVES.getResourceKey());
         }
         if (YungsCaveBiomesCommon.ANCIENT_CAVES_ENABLED) {
-            this.addBiome(mapper, this.temperatures[4], this.FULL_RANGE, Climate.Parameter.span(0.2F, 0.7F), Climate.Parameter.span(0.2F, 1.0F), this.FULL_RANGE, Climate.Parameter.span(0.2F, 0.9F), 0.0F, BiomeModule.ANCIENT_CAVES.getResourceKey());
+            this.addBiome(mapper, CaveBiomeRegionParameters.ANCIENT_CAVES, BiomeModule.ANCIENT_CAVES.getResourceKey());
         }
     }
 }
