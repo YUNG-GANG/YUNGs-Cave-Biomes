935a694ff64ead7d92c0d909fc4a6d7122f301ef
KdotJPG <KdotJPG@users.noreply.github.com>
2022-09-23 23:27:32 -0400

    Tune up Marble Caves terrain reshaping.

--- Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/world/NoiseSamplerBiomeHolder.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/world/NoiseSamplerBiomeHolder.java
@@ -9,10 +9,13 @@ public interface NoiseSamplerBiomeHolder {
     BiomeSource getBiomeSource();
     Registry<Biome> getBiomeRegistry();
     Climate.Sampler getClimateSampler();
+    long getWorldSeed();
 
     void setBiomeSource(BiomeSource source);
 
     void setBiomeRegistry(Registry<Biome> registry);
 
     void setClimateSampler(Climate.Sampler sampler);
+
+    void setWorldSeed(long seed);
 }
