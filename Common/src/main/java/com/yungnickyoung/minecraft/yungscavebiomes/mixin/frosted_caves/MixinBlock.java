88d7a6599a1093649c762026e3ce8aae7ecbc8f5
Nick Young <yungnickyoung@gmail.com>
2024-08-12 14:49:47 -0400

    Improve enchanted ice rendering mixin.

--- Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/mixin/frosted_caves/MixinBlock.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/mixin/frosted_caves/MixinBlock.java
@@ -1,10 +1,12 @@
 package com.yungnickyoung.minecraft.yungscavebiomes.mixin.frosted_caves;
 
 import com.yungnickyoung.minecraft.yungscavebiomes.block.RareIceBlock;
+import com.yungnickyoung.minecraft.yungscavebiomes.module.BlockModule;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,10 +19,15 @@ public abstract class MixinBlock {
      * Prevents rare ice rendering faces when embedded in ice.
      */
     @Inject(method = "shouldRenderFace", at = @At("HEAD"), cancellable = true)
-    private static void yungscavebiomes_cancelRareIceRender(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Direction direction, BlockPos blockPos2, CallbackInfoReturnable<Boolean> cir) {
-        if (blockState.getBlock() instanceof RareIceBlock) {
-            BlockState blockState2 = blockGetter.getBlockState(blockPos2);
-            if (blockState2.isFaceSturdy(blockGetter, blockPos2, direction.getOpposite())) {
+    private static void yungscavebiomes_cancelRareIceRender(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Direction direction, BlockPos neighborPos, CallbackInfoReturnable<Boolean> cir) {
+        if (blockState.is(BlockModule.RARE_ICE.get())) {
+            BlockState neighborBlockState = blockGetter.getBlockState(neighborPos);
+            if (neighborBlockState.isFaceSturdy(blockGetter, neighborPos, direction.getOpposite())) {
+                cir.setReturnValue(false);
+            }
+        } else if (blockState.is(Blocks.ICE)) {
+            BlockState neighborBlockState = blockGetter.getBlockState(neighborPos);
+            if (neighborBlockState.is(BlockModule.RARE_ICE.get())) {
                 cir.setReturnValue(false);
             }
         }
