d80e12bc64e984c51c7be2b10afecc0af8a522cf
Nick Young <yungnickyoung@gmail.com>
2024-09-17 18:43:41 -0400

    Reduce friction when walking on ice sheets.

---
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/mixin/frosted_caves/MixinEntity.java
@@ -0,0 +1,31 @@
+package com.yungnickyoung.minecraft.yungscavebiomes.mixin.frosted_caves;
+
+import com.yungnickyoung.minecraft.yungscavebiomes.module.BlockModule;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.levelgen.Aquifer;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public abstract class MixinEntity {
+    @Shadow private Vec3 position;
+
+    @Shadow public abstract AABB getBoundingBox();
+
+    @Shadow public Level level;
+
+    @Inject(method = "getBlockPosBelowThatAffectsMyMovement", at = @At("HEAD"), cancellable = true)
+    private void yungscavebiomes_applyLowFrictionWhenOnIceSheet(CallbackInfoReturnable<BlockPos> cir) {
+        BlockPos iceSheetPos = new BlockPos(this.position.x, this.getBoundingBox().minY + 0.1, this.position.z);
+        if (this.level.getBlockState(iceSheetPos).is(BlockModule.ICE_SHEET.get())) {
+            cir.setReturnValue(iceSheetPos);
+        }
+    }
+}
