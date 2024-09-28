29206bed4526b7fe63e392aad09b24100b0b2cf5
Nick Young <yungnickyoung@gmail.com>
2024-09-28 19:53:48 -0400

    Add creative tab missing title + entries.

--- Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/module/BlockModule.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/module/BlockModule.java
@@ -225,7 +225,7 @@ public class BlockModule {
 
     public static final TagKey<Block> SAND_SNAPPER_BLOCKS = TagKey.create(Registries.BLOCK,
             YungsCaveBiomesCommon.id("sand_snapper_blocks"));
-    
+
     public static final TagKey<Block> ICE_SHEET_FEATURE_AVOID = TagKey.create(Registries.BLOCK,
             YungsCaveBiomesCommon.id("ice_sheet_feature_avoid"));
 }
--- Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/module/CreativeTabModule.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/module/CreativeTabModule.java
@@ -3,32 +3,45 @@ package com.yungnickyoung.minecraft.yungscavebiomes.module;
 import com.yungnickyoung.minecraft.yungsapi.api.autoregister.AutoRegister;
 import com.yungnickyoung.minecraft.yungsapi.api.autoregister.AutoRegisterCreativeTab;
 import com.yungnickyoung.minecraft.yungscavebiomes.YungsCaveBiomesCommon;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 
 @AutoRegister(YungsCaveBiomesCommon.MOD_ID)
 public class CreativeTabModule {
     @AutoRegister("general")
     public static final AutoRegisterCreativeTab CREATIVE_TAB = AutoRegisterCreativeTab.builder()
+            .title(Component.translatable("itemGroup.yungscavebiomes.general"))
             .iconItem(() -> new ItemStack(BlockModule.LAYERED_ANCIENT_SANDSTONE.get()))
             .entries((params, output) -> {
                 output.accept(BlockModule.ICICLE.get());
                 output.accept(BlockModule.FROST_LILY.get());
                 output.accept(BlockModule.RARE_ICE.get());
                 output.accept(BlockModule.ICE_SHEET.get());
+
                 output.accept(BlockModule.ANCIENT_SAND.get());
                 output.accept(BlockModule.ANCIENT_SANDSTONE.get());
+                output.accept(BlockModule.ANCIENT_SANDSTONE.getStairs());
+                output.accept(BlockModule.ANCIENT_SANDSTONE.getSlab());
+                output.accept(BlockModule.ANCIENT_SANDSTONE.getWall());
                 output.accept(BlockModule.BRITTLE_ANCIENT_SANDSTONE.get());
                 output.accept(BlockModule.LAYERED_ANCIENT_SANDSTONE.get());
                 output.accept(BlockModule.CUT_ANCIENT_SANDSTONE.get());
+                output.accept(BlockModule.CUT_ANCIENT_SANDSTONE.getSlab());
                 output.accept(BlockModule.CHISELED_ANCIENT_SANDSTONE.get());
                 output.accept(BlockModule.SMOOTH_ANCIENT_SANDSTONE.get());
+                output.accept(BlockModule.SMOOTH_ANCIENT_SANDSTONE.getStairs());
+                output.accept(BlockModule.SMOOTH_ANCIENT_SANDSTONE.getSlab());
+
                 output.accept(BlockModule.BRITTLE_SANDSTONE.get());
                 output.accept(BlockModule.LAYERED_SANDSTONE.get());
+
                 output.accept(BlockModule.BRITTLE_RED_SANDSTONE.get());
                 output.accept(BlockModule.LAYERED_RED_SANDSTONE.get());
+
                 output.accept(BlockModule.PRICKLY_PEACH_CACTUS.get());
                 output.accept(ItemModule.PRICKLY_PEACH_ITEM.get());
                 output.accept(BlockModule.PRICKLY_VINES.get());
+
                 output.accept(ItemModule.ICE_CUBE_SPAWN_EGG.get());
                 output.accept(ItemModule.SAND_SNAPPER_SPAWN_EGG.get());
             })
