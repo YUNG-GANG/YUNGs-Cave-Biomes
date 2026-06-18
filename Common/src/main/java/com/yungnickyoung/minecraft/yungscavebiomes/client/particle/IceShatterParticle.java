818f30fafdc0a6eb9faddec11bd6e428e97e023c
williambl <william@williambl.com>
2026-06-18 13:23:31 +0100

    Fix ice shatter particle having wrong UVs

--- Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/client/particle/IceShatterParticle.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/yungscavebiomes/client/particle/IceShatterParticle.java
@@ -33,26 +33,6 @@ public class IceShatterParticle extends SingleQuadParticle {
     }
 
     @Override
-    protected float getU0() {
-        return this.sprite.getU((this.uo + 1.0F) / 4.0F * 16.0F);
-    }
-
-    @Override
-    protected float getU1() {
-        return this.sprite.getU(this.uo / 4.0F * 16.0F);
-    }
-
-    @Override
-    protected float getV0() {
-        return this.sprite.getV(this.vo / 4.0F * 16.0F);
-    }
-
-    @Override
-    protected float getV1() {
-        return this.sprite.getV((this.vo + 1.0F) / 4.0F * 16.0F);
-    }
-
-    @Override
     public int getLightCoords(float f) {
         int lightAtPos = super.getLightCoords(f);
         int k = lightAtPos >> 16 & 0xFF;
